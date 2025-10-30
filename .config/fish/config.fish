4bae9dda682c4485a12886303b882f68cf1a6fd7
Mariam Askarpour <laikamari@fastmail.org>
2025-10-30 17:19:06 -0400

    switch to fish, adapt bash config to fish, update ghostty config

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+################
+# SHELL CONFIG #
+################
+
+# bring programs into path
+set -gx PATH '~/.local/bin:'"$PATH"
+
+# set up package managers
+source ~/.cargo/env.fish
+set -gx GOPATH "$HOME"'/.go'
+set -gx NVM_DIR "$HOME"'/.nvm'
+
+# starship prompt
+source (/usr/local/bin/starship init fish --print-full-init | psub)
+
+set -gx EDITOR hx
+
+alias please='sudo '(fc -ln -1 | string collect; or echo)
+alias l='eza'
+alias la='eza -a'
+alias ll='eza -l'
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias dc='docker compose'
+alias yy='yadm add -u && yadm commit && yadm push'
